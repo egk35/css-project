c6da748e5a5e5d184bc10b41152fcc29379496d4
Ethan Goldman-Kirst <ethangk23@gmail.com>
2016-12-26 14:35:02 -0800

    shrinking loading spinner

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Hello World!</title>
+    <link href="css/icons.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <div class="iconGroup">
+      <div>
+        <div class="shrinkingSpinner" />
+      </div>
+    </div>
+  </body>
+</html>
